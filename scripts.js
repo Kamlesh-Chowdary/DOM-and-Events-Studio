c537c2d8f83783a3d72aba85495529ba9dc16e32
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2023-09-22 09:18:50 -0700

    Added event listener to takeoff button

--- scripts.js
+++ scripts.js
@@ -8,5 +8,20 @@ function load() {
   const down = document.querySelector("#down");
   const left = document.querySelector("#left");
   const right = document.querySelector("#right");
+  const statuss = document.querySelector("#flightStatus");
+  const screen = document.querySelector("#shuttleBackground");
+  const height = document.querySelector("#spaceShuttleHeight");
+
+  takeOff.addEventListener("click", () => {
+    let isReady = window.confirm(
+      "Confirm that the shuttle is ready for takeoff."
+    );
+    if (isReady) {
+      statuss.innerHTML = "Shuttle in flight.";
+      screen.style.backgroundColor = "blue";
+      height.innerHTML = "10000";
+    }
+  });
 }
+
 window.addEventListener("load", load);
