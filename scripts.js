23a7988ba3f2445e5b1929e7620ee3572ebffa01
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2023-09-22 09:07:26 -0700

    Added onload event listener

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function load() {
+  const takeOff = document.querySelector("#takeoff");
+  const landing = document.querySelector("#landing");
+  const missionAbort = document.querySelector("#missionAbort");
+  const up = document.querySelector("#up");
+  const down = document.querySelector("#down");
+  const left = document.querySelector("#left");
+  const right = document.querySelector("#right");
+}
+window.addEventListener("load", load);
